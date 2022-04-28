c3d6116c48dd61fd8322ff17fdde241acd71327a
Ji Woong Park <jiwoongpark@Jis-iMac.local>
2022-04-28 19:22:58 -0400

    Added logistic regression model

--- training.py
+++ training.py
@@ -15,18 +15,25 @@ with open('config.json','r') as f:
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 model_path = os.path.join(config['output_model_path']) 
 
-
+datalocation = os.getcwd() + '/' + dataset_csv_path + '/finaldata.csv'
+trainingdata = pd.read_csv(datalocation)
 #################Function for training the model
 def train_model():
     
+    X = trainingdata.iloc[:,1:-1].values # excludes the last column
+    y = trainingdata['exited']
+    
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+    lr = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
                     intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
+                    multi_class='auto', n_jobs=None, penalty='l2',
                     random_state=0, solver='liblinear', tol=0.0001, verbose=0,
                     warm_start=False)
     
     #fit the logistic regression to your data
+    lr.fit(X,y)
     
     #write the trained model to your workspace in a file called trainedmodel.pkl
+    pickle.dump(lr, open(model_path+'/trainedmodel.pkl', 'wb'))
 
+train_model()
