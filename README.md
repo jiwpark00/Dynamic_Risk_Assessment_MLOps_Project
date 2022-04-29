c5e6a941dab8677b95fd995c64b63ee6f0916cdb
Ji Woong Park <jiwoongpark@Jis-iMac.local>
2022-04-28 21:48:43 -0400

    Added diagnostics and updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 ## This is the fourth and last project in Machine Learning DevOps Nanodegree.
 
 The goal here is to take a model and mimic the end-to-end process, including scenarios for re-training and checking for potential drifts as expected during model build and deployment, and release.
+
+# Data Ingestion
+The file ingestion.py combines the two data by reading in the directory, combines them, removes duplicates, and then saves it.
+
+# Training, Scoring, and Deployment
+Training is done via training.py file. The output model file is in the pickle format. This model is not hyperparameter tuned.
+
+Scoring is done to write the F1 score to a .txt file for the latestscore measured.
+
+Deployment takes the saved model, ingested file, and F1 score into the production env directory.
+
+# Diagnostics
+There is a single diagnostics.py file that does a quite bit of work here.
+
+- This file checks to ensure that the deployed model can score on the test dataset.
+
+- This file also measures the performance of data ingestion and training and report this.
+
+- Summary statistics for the numerical columns are calculated for mean, median, and standard deviation.
+
+- Dependency statuses are carefully evaluated for currently being used by the project (from Udacity env), installed version, and the most recent version as called by on Apr 2022.
--- diagnostics.py
+++ diagnostics.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 import timeit
 import os
+import pickle
 import json
+import requests
+import subprocess
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
@@ -12,31 +15,98 @@ with open('config.json','r') as f:
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 test_data_path = os.path.join(config['test_data_path']) 
 
+full_test_data_path = os.getcwd() + '/' + test_data_path + '/testdata.csv'
+test_df = pd.read_csv(full_test_data_path)
+
+model_path = os.path.join(config['prod_deployment_path']) 
+lr = pickle.load(open(model_path+'/trainedmodel.pkl', 'rb'))
+
+dataset_path = os.getcwd() + '/' + dataset_csv_path + '/finaldata.csv'
+dataset_for_stats = pd.read_csv(dataset_path)
+numeric_cols = dataset_for_stats.columns[1:-1] # excluding first column and last column that's actually a target
+
 ##################Function to get model predictions
-def model_predictions():
+def model_predictions(df, model):
     #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    preds = model.predict(df.iloc[:,1:-1].values)
+
+    return preds #return value should be a list containing all predictions
 
 ##################Function to get summary statistics
 def dataframe_summary():
     #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    metric_list = []
+    
+    for col in numeric_cols:
+        col_vals = {}
+        col_vals['column_name'] = col
+        col_vals['mean'] = np.mean(dataset_for_stats[col].values)
+        col_vals['median'] = np.median(dataset_for_stats[col].values)
+        col_vals['std'] = np.std(dataset_for_stats[col].values)
+        
+        metric_list.append(col_vals)
+
+    return metric_list #return value should be a list containing all summary statistics
+
+def check_NA():
+    # measure the percentage of NA values in each numeric dataset columns
+    for col in numeric_cols:
+        nas=list(dataset_for_stats.isna().sum())
+        napercents=[nas[i]/len(dataset_for_stats.index) for i in range(len(nas))]
+    
+    return napercents
 
 ##################Function to get timings
 def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    starttime = timeit.default_timer()
+    os.system('python ingestion.py')
+    ingestion_time = timeit.default_timer() - starttime
+    
+    starttime = timeit.default_timer()
+    os.system('python training.py')
+    training_time = timeit.default_timer() - starttime    
+    
+    #return a list of 2 timing values in seconds
+    return ingestion_time, training_time
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    #get a list of details for each package
+    # shows the name of Python module being used - from requirements
+    requirements = pd.read_csv('requirements.txt',sep="==",header=None)
+    requirements.columns = ['package_name', 'current_version']
+    
+    # shows the current installed Python module - from pip list
+    installed = subprocess.check_output(['pip', 'list', '--format', 'json'])
+    parsed_results = json.loads(installed)
+    installed_df = pd.DataFrame([(element["name"], element["version"]) for element in parsed_results])
+    
+    installed_df.columns = ['package_name','installed_version']
+    requirements_installed = pd.merge(requirements, installed_df, how='inner', left_on=['package_name'],right_on=['package_name'])
+    
+    # the most recent python module available - from pip
 
+    most_recent = []
 
-if __name__ == '__main__':
-    model_predictions()
-    dataframe_summary()
-    execution_time()
-    outdated_packages_list()
+    for package in list(requirements_installed['package_name']):
+        response = requests.get(f'https://pypi.org/pypi/{package}/json')
+        version = response.json()['info']['version']
+        most_recent.append((package, version))
+    
+    most_recent_df = pd.DataFrame(most_recent)
+    most_recent_df.columns = ['package_name','most_recent_version']
+    requirements_installed_recent = pd.merge(requirements_installed, most_recent_df, how='inner', left_on=['package_name'],right_on=['package_name'])    
+    
+    # we will return this to the production deployment
+    output_path_dependencies = os.getcwd() + '/' + os.path.join(config['prod_deployment_path']) + '/dependencies_status.csv'
+    requirements_installed_recent.to_csv(output_path_dependencies,index=False)
+
+model_predictions(test_df,lr)
+dataframe_summary()
+check_NA()
+execution_time()
+outdated_packages_list()
 
 
 
